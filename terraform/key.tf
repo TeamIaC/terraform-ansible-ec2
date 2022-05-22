dbf5c12157edada17bfa273ab8a0ec5fcd302f0a
ymktmk <73768462+ymktmk@users.noreply.github.com>
2022-05-22 19:04:52 +0900

    [add] ansible

--- terraform/key.tf
+++ terraform/key.tf
@@ -3,7 +3,7 @@ resource "aws_key_pair" "key_pair" {
       # 現時点ではユーザー側でキーペア生成しなければならない
       # ssh-keygen -t rsa -f example -N ''
       # ssh -i ./example ec2-user@35.77.206.159
-      public_key = file("./example.pub")
+      public_key = file("../example.pub")
 }
 
 resource "aws_eip" "ec2_eip" {
